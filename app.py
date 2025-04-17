056a2368e875ed8bf1450619837ef2eaeaa2e976
tinti400 <seuemail@exemplo.com>
2025-04-16 22:30:50 -0300

    Corrige menu lateral multipage

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
+import os
 
-st.set_page_config(page_title="LigaFut", layout="wide")
-
-# Título inicial
-st.markdown(
-    """
-    <div style='text-align: center; padding-top: 100px;'>
-        <h1 style='font-size: 3em;'>⚽ LigaFut</h1>
-        <p style='font-size: 1.2em;'>Gerencie sua liga como um verdadeiro técnico!</p>
-    </div>
-    """,
-    unsafe_allow_html=True
+# Deixe o título e fundo como quiser
+st.set_page_config(
+    page_title="LigaFut",
+    page_icon="⚽",
+    layout="wide"
 )
+
+# Conteúdo da Home
+st.markdown("<h1 style='text-align: center;'>⚽ LigaFut</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Gerencie sua liga como um verdadeiro técnico!</h4>", unsafe_allow_html=True)
+
+# Força exibição do menu lateral com as páginas
+st.sidebar.success("Selecione uma opção acima ☝️")
